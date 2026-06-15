9f3b9e37bad70c79013d34c2b81ce04f828a8a9c
Ramana Nallabothula <ramananallabothula3126@gmail.com>
2026-06-15 15:20:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 import streamlit as st
 
-st.title("NIAT RAG Challenge")
+st.set_page_config(page_title="NIAT RAG Challenge")
 
-def ask_bot(question):
-    return f"You asked: {question}"
+st.title("NIAT RAG Challenge")
 
 question = st.text_input("Ask a question")
 
 if question:
-    answer = ask_bot(question)
-    st.write(answer)
+    st.write("Bot is working")
